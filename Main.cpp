628854a9a5038b90d8191094b4816884a714d464
alice <aliseklg@gmail.com>
2025-10-31 20:23:45 +0300

    add task_04_03

---
+++ Main.cpp
@@ -0,0 +1,31 @@
+//
+// Created by alice on 31.10.2025.
+//
+#include <vector>
+#include <cassert>
+
+template<typename Container, typename... Args>
+void push_back_ints(Container &container, Args... args) {
+
+    ((container.push_back(args)), ...);
+}
+
+int main() {
+    std::vector<int> vec;
+
+    push_back_ints(vec, 1, 2, 3, 4, 5);
+    assert(vec.size() == 5);
+    assert(vec[0] == 1 && vec[1] == 2 && vec[2] == 3 && vec[3] == 4 && vec[4] == 5);
+    assert(vec.back() == 5);
+
+    std::vector<int> vec2;
+    push_back_ints(vec2, 42);
+    assert(vec2.size() == 1);
+    assert(vec2[0] == 42);
+
+    std::vector<int> vec3;
+    push_back_ints(vec3);
+    assert(vec3.empty());
+
+    return 0;
+}

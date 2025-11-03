4ea571176b16b0caef1cd052f170f1fbdfed3f72
alice <aliseklg@gmail.com>
2025-11-03 13:30:23 +0300

    fix task_03_05

--- Main.cpp
+++ Main.cpp
@@ -20,22 +20,24 @@ public:
 
 class Adapter_v1 : public Entity_v1 {
 public:
-    virtual ~Adapter_v1() = default;
+    ~Adapter_v1() override = default;
 
     virtual void test_v1() = 0;
 
-    void test() override final {
+    void test() final {
+        std::cout << "1111" << std::endl;
         test_v1();
     }
 };
 
 class Adapter_v2 : public Entity_v2 {
 public:
-    virtual ~Adapter_v2() = default;
+    ~Adapter_v2() override = default;
 
     virtual void test_v2() = 0;
 
-    void test() override final {
+    void test() final {
+        std::cout << "222" << std::endl;
         test_v2();
     }
 };
@@ -43,11 +45,11 @@ public:
 class Client : public Adapter_v1, public Adapter_v2 {
 public:
     void test_v1() override {
-        std::cout << "Client::test_v1() - specialized implementation for Entity_v1" << std::endl;
+        std::cout << "Client::test_v1() - специальная реализация для Entity_v1" << std::endl;
     }
 
     void test_v2() override {
-        std::cout << "Client::test_v2() - specialized implementation for Entity_v2" << std::endl;
+        std::cout << "Client::test_v2() - специальная реализация для Entity_v2" << std::endl;
     }
 };
 
@@ -57,13 +59,13 @@ int main() {
     Entity_v1 *entity1 = &client;
     Entity_v2 *entity2 = &client;
 
-    std::cout << "Calling through Entity_v1 pointer:" << std::endl;
+    std::cout << "Обращаемся через указатель Entity_v1:" << std::endl;
     entity1->test();
 
-    std::cout << "Calling through Entity_v2 pointer:" << std::endl;
+    std::cout << "Обращаемся через указатель Entity_v2:" << std::endl;
     entity2->test();
 
-    std::cout << "Calling directly through Client:" << std::endl;
+    std::cout << "Обращаемся через Client:" << std::endl;
     client.test_v1();
     client.test_v2();
 
